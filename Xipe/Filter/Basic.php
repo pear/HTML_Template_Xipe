1d495f88c94ad393b4759d5d18eeac5ba4e71940
?? McCain <mccain@php.net>
2002-04-15 20:23:44 +0000

    - removed translate stuff
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_Xipe/trunk@78278 c90b9560-bf6c-de11-be94-00142212c4b1

--- Xipe/Filter/Basic.php
+++ Xipe/Filter/Basic.php
@@ -19,6 +19,9 @@
 /**
 *
 *   $Log$
+*   Revision 1.4  2002/03/22 19:48:24  mccain
+*   - fixed a bug in the applyTranslate... now also method calls work
+*
 *   Revision 1.3  2002/02/07 22:45:55  mccain
 *   - make the options stuff work
 *
@@ -341,53 +344,6 @@ but this works:
                             "foreach($2 ",
 
                             $input);
-    }
-
-    /**
-    *   apply a function/method to each output which translates the string, so i dont have to
-    *   do it by hand every time
-    *   use only as POST-filter, since it looks for '< ? =' tags
-    *
-    *   NOTE:   its quite couragous using this filter, since it might also translate usernames or other stuff
-    *           that is dynamically generated, and if it also is in the translate-table then the page may become funny
-    *
-    *   @version    02/01/08
-    *   @author     Wolfram Kriesing <wolfram@kriesing.de>
-    *   @param      string  $input  the original template code
-    *   @param      string  $functionName   the function to put around a string that is printed
-    *   @param      array   $possibleMarkUpDelimiters   delimiters that are around a text that should be translated, see Language_Translate
-    *   @see        $Language_Translate::possibleMarkUpDelimiters
-    *   @return     string  the modified template
-    */
-    function applyTranslateFunction( $input , $functionName , $possibleMarkUpDelimiters )
-    {
-        if( sizeof($possibleMarkUpDelimiters) )
-        foreach( $possibleMarkUpDelimiters as $begin=>$end )  // go thru all the delimiters and try to translate the strings
-        {
-# FIXXME the [ ^ ? > ] makes it impossible to translate the following string
-# automatically: < ?=$key? > --- < ?=$aLoop? >
-# if i had left a "." instead there it would result in: < ?=translateMathod($key? > --- < ?=$aLoop)? >
-# but now we have the problem that ONLY to stuff inside the $possibleMarkUpDelimiters
-# the translate function is applied, but we need the $possibleMarkUpDelimiters since
-# we dont want to translate every < ?=.. tag, since those might also just be formatting
-# things or in a style sheet simply a path or whatsoever, so $possibleMarkUpDelimiters IS DEFINITELY NEEDED
-# but must become better
-
-            $input = preg_replace(
-                                    // $ [^ ? >]    takes care of only applying the method to the proper block, i think there is some reg-exp modifier for that too, but dont know yet
-                                    // (->)?        takes care of class operators to be included in the translation
-                                    '/('.$begin.'<\?=)(\$([^?>](->)?)*)(\?>'.$end.')/i' ,
-                                    "$1$functionName($2)$5" ,
-                                    $input );
-        }
-        return $input;
-
-/*
-    TEST CASES THAT PASSED:
-    1. the problem here was the class-operator '->', since the '>' is also in the possibleMarkUpDelimiter
-    <td class="listContent">< ?=$language->getName($aBookmark['language'])? ></td>
-
-*/
     }
 
     /**
